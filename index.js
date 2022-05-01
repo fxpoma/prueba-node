b63e2135a59b4c0fb6f91d80a316fb669011a8c4
Pancho <francisco.poma40ta@gmail.com>
2022-05-01 14:03:51 -0500

    Este es el commit 1

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ const app = express();
 
 app.get('/',(req,res)=> res.send('<h1>Babas en express</h1>'))
 
+console.log("Esto solo es una baba");
+
 app.listen(8080);
 console.log('Server on port 8080')
