2cb95a5bee379952b463a138a4b8bef983f67194
Pancho <francisco.poma40ta@gmail.com>
2022-05-01 14:09:20 -0500

    Esta se supone que debería ser la segunda baba

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.get('/',(req,res)=> res.send('<h1>Babas en express</h1>'))
 
-console.log("Esto solo es una baba");
+console.log("Se supone que no debería ser así");
 
 app.listen(8080);
 console.log('Server on port 8080')
