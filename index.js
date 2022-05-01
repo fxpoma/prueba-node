8977a0f1671c603bdfc04f7f8b82c8c8c6386471
Pancho <francisco.poma40ta@gmail.com>
2022-05-01 14:06:38 -0500

    Este es el segundo commit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+console.log("La baba no debería estar aquí");
+
 app.get('/',(req,res)=> res.send('<h1>Babas en express</h1>'))
 
 app.listen(8080);
